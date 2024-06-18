71b1cece3a9956e06b527a7adc236ec243eb1e13
tanujjoshi26 <52505586+tanujjoshi26@users.noreply.github.com>
2024-06-18 20:05:46 +0530

    Add files via upload

---
+++ folder_listing.py
@@ -0,0 +1,13 @@
+import os
+folders = input("Enter folder name with common spacing :").split()
+
+try:
+   for folder in folders :
+      files = os.listdir(folder)
+      print ("Files in the following folder - " + folder)
+
+   for file in files:
+      print(file)
+
+except FileNotFoundError:
+   print("Please provide correct path")
